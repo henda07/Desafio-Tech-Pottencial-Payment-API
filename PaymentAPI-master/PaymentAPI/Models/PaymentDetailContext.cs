a6fd7405535dd927545f54d1786e67e1d9c9f0f4
henda07 <96376694+henda07@users.noreply.github.com>
2022-09-20 18:02:42 +0100

    Add files via upload

---
+++ PaymentAPI-master/PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> paymentDetailContext) : base(paymentDetailContext) { }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
---
+++ PaymentAPI-master/PaymentAPI/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using PaymentAPI.Models;
+
+namespace PaymentAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentAPI", Version = "v1" });
+            });
+
+            services.AddDbContext<PaymentDetailContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentAPI v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
